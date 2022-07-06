995798b65b25c2392648bf48e42b23b38f4f0a9d
Puneeth <puneeth94@gmail.com>
2022-07-06 16:35:22 +0530

    Adds namespace as variable

--- parser-script.sh
+++ parser-script.sh
@@ -1,12 +1,21 @@
 #!/bin/bash
+set -e
 # declare STRING variable
 STRING="Initializing dependency parser"
 # print variable on a screen
 echo $STRING
 
-DEVSTACK_LABEL=$1
+if [ $# -lt 2 ]
+  then
+    echo "Please provide devstack label and namespace as args. ex -
+    devspace list-deps <devstack_label> <namespace>"
+    exit 1
+fi
 
-CONFIGMAP_NAME=$(kubectl get job -n pg-router -l devstack_label=${DEVSTACK_LABEL} -l psvh=true -o yaml \
+DEVSTACK_LABEL=$1
+NAMESPACE=$2
+
+CONFIGMAP_NAME=$(kubectl get job -n ${NAMESPACE} -l devstack_label=${DEVSTACK_LABEL} -l psvh=true -o yaml \
 | yq eval '.items[0].spec.template.spec.volumes[0].configMap.name')
 
 
